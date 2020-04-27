699dab0c37157c369b4b3baa602992a3ace7b1f6
Edgar Rojas <edgarjrojasl@gmail.com>
2020-04-27 18:34:13 -0400

    Updates

---
+++ precache-manifest.a4ad329c92c08c4ff81ec612396e4730.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823dab3a78a85d49939bda3c7fadc3b0",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "eacba9036072e327799d",
+    "url": "/react-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "89fee4136ab921c9a5d3",
+    "url": "/react-app/static/js/2.c106f3b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.c106f3b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eacba9036072e327799d",
+    "url": "/react-app/static/js/main.bbe813a2.chunk.js"
+  },
+  {
+    "revision": "f672e53d1d80f5d29a34",
+    "url": "/react-app/static/js/runtime-main.1389f889.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
